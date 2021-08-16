7306e9bf2b654e2b6b8023529815b6b60087900c
rgx6 <rgx@hotmail.co.jp>
2021-08-17 00:31:06 +0900

    gulpfile.js修正。

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('img', function () {
             'src/img/**/*.*',
             '!src/img/sprite_src/*.*',
             '!src/img/stamp_original/*.*',
-            '!src/img/stamp_twitter_optimized/*.*',
+            '!src/img/stamp_twitter_optimized/*.*'
         ])
         .pipe(gulp.dest('src/public/images'));
 });
