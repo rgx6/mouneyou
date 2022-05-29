59c330ad6d98d7da28edc0273fc83062c451a795
rgx6 <rgx@hotmail.co.jp>
2022-05-29 17:13:59 +0900

    twitter_server.js 仮修正

--- dest/twitter_server.js
+++ dest/twitter_server.js
@@ -1,36 +1,38 @@
-var Twitter = require('twitter');
-var Promise = require('es6-promise').Promise;
-var log4js  = require('log4js');
-var db      = require('./db.js');
-var config  = require('./twitter_configuration.js');
+const Twitter = require('twitter');
+const Promise = require('es6-promise').Promise;
+const log4js = require('log4js');
+const db = require('./db.js');
+const config = require('./twitter_configuration.js');
 
 log4js.configure('log4js_configuration.json', { reloadSecs: 60 });
 
-var logger = log4js.getLogger('twitterLog');
-logger.setLevel(log4js.levels.INFO);
-// logger.setLevel(log4js.levels.DEBUG);
+const logger = log4js.getLogger('twitterLog');
 
-var client;
+let client;
 
-var queryUrl            = 'mouneyou.rgx6.com';
-var queryCount          = 100;
-var queryDefaultSinceId = '665564226052751360';
+const queryUrl = 'mouneyou.rgx6.com';
+const queryCount = 100;
+const queryDefaultSinceId = '665564226052751360';
 
-var searchQuery = {
-    q:        queryUrl,
-    count:    queryCount,
+const searchQuery = {
+    q: queryUrl,
+    count: queryCount,
     since_id: queryDefaultSinceId,
 };
 
-var searchInterval = 10 * 1000;
+const searchInterval = 10 * 1000;
 
-var isSearching = false;
+let isSearching = false;
 
-var apiErrorLimit = 10;
-var apiErrorCount = 0;
+const apiErrorLimit = 10;
+let apiErrorCount = 0;
 
-// log4jsの初期化が間に合わないようなので少し待つ
-setTimeout(function () {
+main();
+
+async function main() {
+    // log4jsの初期化が間に合わないようなので少し待つ
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
     logger.info('start');
 
     client = new Twitter(config);
@@ -38,7 +40,7 @@ setTimeout(function () {
     logger.debug('search start');
     getTweets();
 
-    var timer = setInterval(function () {
+    let timer = setInterval(function () {
         if (apiErrorLimit <= apiErrorCount) {
             logger.error('stop api error count is over');
             clearInterval(timer);
@@ -50,20 +52,20 @@ setTimeout(function () {
         } else {
             logger.debug('search start');
             // since_idはそのままだけどmax_idは毎回初期化する
-            delete(searchQuery.max_id);
+            delete (searchQuery.max_id);
             setQuerySinceId().then(getTweets);
         }
     }, searchInterval);
-}, 1000);
+}
 
-function setQuerySinceId () {
+function setQuerySinceId() {
     'use strict';
     logger.debug('setQuerySinceId');
 
     return new Promise(function (fulfill, reject) {
         isSearching = true;
 
-        var query = db.Tweet.findOne().select({ tweetId: 1 }).sort({ createdAt: 'desc', tweetId: 'desc' });
+        let query = db.Tweet.findOne().select({ tweetId: 1 }).sort({ createdAt: 'desc', tweetId: 'desc' });
         query.exec(function (err, doc) {
             if (err) {
                 logger.error(err);
@@ -82,7 +84,7 @@ function setQuerySinceId () {
     });
 }
 
-function getTweets (max_id_str) {
+function getTweets(max_id_str) {
     'use strict';
     logger.debug('getTweets', max_id_str);
 
@@ -100,13 +102,13 @@ function getTweets (max_id_str) {
 
         apiErrorCount = 0;
 
-        var nextMaxId;
+        let nextMaxId;
 
         tweets.statuses.forEach(function (tweet, index, array) {
             nextMaxId = tweet.id_str;
 
-            var mediaUrls = [];
-            var expandedUrls = [];
+            let mediaUrls = [];
+            let expandedUrls = [];
             if (tweet.entities != null && tweet.entities.media != null) {
                 tweet.entities.media.forEach(function (media, index, array) {
                     if (media.media_url != null) mediaUrls.push(media.media_url);
@@ -120,12 +122,12 @@ function getTweets (max_id_str) {
                     return;
                 }
 
-                var data = new db.Tweet();
-                data.tweetId        = tweet.id_str;
+                let data = new db.Tweet();
+                data.tweetId = tweet.id_str;
                 data.userScreenName = tweet.user.screen_name;
-                data.mediaUrls      = mediaUrls;
-                data.expandedUrls   = expandedUrls;
-                data.createdAt      = Date.parse(tweet.created_at);
+                data.mediaUrls = mediaUrls;
+                data.expandedUrls = expandedUrls;
+                data.createdAt = Date.parse(tweet.created_at);
                 data.registeredTime = new Date();
                 data.save(function (err, doc) {
                     if (err) {
@@ -138,7 +140,7 @@ function getTweets (max_id_str) {
             });
         });
 
-        var count = tweets.statuses.length;
+        let count = tweets.statuses.length;
         logger.debug('count: ', count);
         if (count <= 1) {
             logger.debug('search end');
